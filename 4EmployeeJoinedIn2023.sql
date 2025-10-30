bc689fb787ec0937da58ee73edb78fd333f2b8af
saujanyamahajan <saujanya.9919@gmail.com>
2025-10-30 22:06:56 +0530

    Get employees who joined in the last 90 days

--- 4EmployeeJoinedIn2023.sql
+++ 4EmployeeJoinedIn2023.sql
@@ -1,2 +1,9 @@
 select * from employee
 where year(joiningDate)=2025
+
+
+
+--Get employees who joined in the last 90 days.
+
+select * from employee
+where cast(joiningDate as date)>= cast( dateadd(date,-90,getDate()) as date )

0f7cf5478adc431a5098bd81a32a16fa5ab280f0
saujanyamahajan <saujanya.9919@gmail.com>
2025-10-31 00:42:02 +0530

    DUP ON TABLE

--- 2DupNameinTable.sql
+++ 2DupNameinTable.sql
@@ -4,12 +4,21 @@ group by name
 having Count(*)>1
 
 
-'5. List all duplicate records in a table.'
-'aggregate (sum ,avg, count) doesnt work in where '
-'having is needed'
+--'5. List all duplicate records in a table.'
+--'aggregate (sum ,avg, count) doesnt work in where '
+--'having is needed'
 
 
 select id,name,department,Count(*) 
 from employees
 group by id,name,department
 having Count(*) >1
+
+
+
+--DUP IN TABLE 
+
+SELECT ID,department,NAME,Count(*) 
+FROM employee
+group by ID,department,NAME
+having Count(*)>1

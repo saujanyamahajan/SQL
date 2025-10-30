3ca8583d740d7bd49fb8d520fab28e96e7f11340
saujanyamahajan <saujanya.9919@gmail.com>
2025-10-31 01:02:43 +0530

    Find common employees in two tables Employee_A and Employee_B

--- 9RelationBWEmp&Manager.sql
+++ 9RelationBWEmp&Manager.sql
@@ -1,5 +1,12 @@
 
---
+--Write a query to show manager → employee relationship.
 select * from employee e
 inner join manager m
 on e.id=m.id
+
+
+
+--Find common employees in two tables Employee_A and Employee_B.
+select * from Employee_A a
+inner join Employee_B b
+on a.id=b.id

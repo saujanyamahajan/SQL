39bc61d06a6afb3feaecdd2e59ef71c385698542
saujanyamahajan <saujanya.9919@gmail.com>
2025-10-30 21:59:52 +0530

    List departments having more than 5 employees

--- 7ExpensePerDepartment.sql
+++ 7ExpensePerDepartment.sql
@@ -3,3 +3,11 @@
 select department,sum(salary) as totalSalary
 from employee
 group by department
+
+
+
+--List departments having more than 5 employees.
+
+select department,count(employeeid) as totalEmployees
+from employees
+group by department

1bad0191ff8915bdb72aa1599c393349755de5c7
saujanyamahajan <saujanya.9919@gmail.com>
2025-10-31 08:36:17 +0530

    3rd highest salary using DENSE_RANK() or ROW_NUMBER()

--- 1SecondHighest.sql
+++ 1SecondHighest.sql
@@ -12,3 +12,20 @@ where Salary<(select max(salary)from employee where salary<select * from employe
 ---Find employees with salaries greater than the average salary.
 
 select * from employees where salary > (select avg(salary) from employees)
+
+
+
+
+---3rd highest salary using DENSE_RANK() or ROW_NUMBER()
+create table #DENSE_RANK
+{
+empId int;
+name varchar(50);
+department varchar(50)
+salary int;
+}
+
+insert into #rank(empId,name,department,salary)
+(select employeeid,name,department,salary 
+DENSE_RANK() over (order by salary desc)
+from employee  )

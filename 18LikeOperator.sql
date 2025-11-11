9e00e5461d5df827b58e21308fd787967e9ea9cc
saujanyamahajan <saujanya.9919@gmail.com>
2025-11-11 18:12:59 +0530

    Get all employee detail from EmployeeDetail table whose FirstName containing '%'. ex:-Vik%as

--- 18LikeOperator.sql
+++ 18LikeOperator.sql
@@ -14,4 +14,13 @@ SELECT * FROM [EmployeeDetail] WHERE FirstName like '[^a-p]%'
 -- and contain 4 letters. The Underscore(_) Wildcard Character represents any single
 -- character.
 
-SELECT * FROM [EmployeeDetail] WHERE Gender like ' le' --there are two "_"
+SELECT * FROM [EmployeeDetail] WHERE Gender like '__le' --there are two "_"
+
+-- Get all employee detail from EmployeeDetail table whose "FirstName" start with
+-- 'A' and contain 5 letters.
+SELECT * FROM [EmployeeDetail] WHERE FirstName like 'A '
+--there are four "_"
+
+-- Get all employee detail from EmployeeDetail table whose "FirstName"
+-- containing '%'. ex:-"Vik%as".
+SELECT * FROM [EmployeeDetail] WHERE FirstName like '%[%]%'

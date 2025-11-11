ea79af3baec1c3d4c5101534d7ea9b8c1b1f79b7
saujanyamahajan <saujanya.9919@gmail.com>
2025-11-11 18:10:28 +0530

    string ques

--- 18LikeOperator.sql
+++ 18LikeOperator.sql
@@ -9,3 +9,9 @@ SELECT * FROM EmployeeDetail WHERE FirstName like '[a-p]%'
 -- with any single character between 'a-p'
 
 SELECT * FROM [EmployeeDetail] WHERE FirstName like '[^a-p]%'
+
+-- Get all employee detail from EmployeeDetail table whose "Gender" end with 'le'
+-- and contain 4 letters. The Underscore(_) Wildcard Character represents any single
+-- character.
+
+SELECT * FROM [EmployeeDetail] WHERE Gender like ' le' --there are two "_"

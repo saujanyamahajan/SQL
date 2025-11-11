170c1659119fc81d4f4c1556ca633db3d6aeac4d
saujanyamahajan <saujanya.9919@gmail.com>
2025-11-11 17:48:07 +0530

    Get all employee detail from EmployeeDetail table whose FirstName not start with any single character between 'a-p'

--- 18LikeOperator.sql
+++ 18LikeOperator.sql
@@ -3,3 +3,9 @@
 
 
 SELECT * FROM EmployeeDetail WHERE FirstName like '[a-p]%'
+
+
+-- Get all employee detail from EmployeeDetail table whose "FirstName" not start
+-- with any single character between 'a-p'
+
+SELECT * FROM [EmployeeDetail] WHERE FirstName like '[^a-p]%'

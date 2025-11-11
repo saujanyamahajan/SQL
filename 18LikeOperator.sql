45be6a8b134021c42016526626d99a3f6ad1fb4b
saujanyamahajan <saujanya.9919@gmail.com>
2025-11-11 17:45:26 +0530

    Get all employee detail from EmployeeDetail table whose FirstName start with any single character between 'a-p'

---
+++ 18LikeOperator.sql
@@ -0,0 +1,5 @@
+--Get all employee detail from EmployeeDetail table whose "FirstName" start with
+--any single character between 'a-p'
+
+
+SELECT * FROM EmployeeDetail WHERE FirstName like '[a-p]%'

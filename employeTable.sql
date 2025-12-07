fe791420f5b9b2f18375d041c28893fe29807d82
saujanyamahajan <saujanya.9919@gmail.com>
2025-12-07 10:00:00 +0530

    date ques

---
+++ employeTable.sql
@@ -0,0 +1,22 @@
+INSERT INTO Employees
+(
+    FirstName,
+    LastName,
+    Email,
+    Phone,
+    Department,
+    Designation,
+    DateOfJoining,
+    IsActive
+)
+VALUES
+(
+    'Saujanya',
+    'Mahajan',
+    'saujanya.mahajan@email.com',
+    '9876543210',
+    'IT',
+    'Senior Software Developer',
+    '2024-11-01',
+    1
+);

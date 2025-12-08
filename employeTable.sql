44f171eedc523f147370c3ccc9b3b5d557b6a936
saujanyamahajan <saujanya.9919@gmail.com>
2025-12-08 10:00:00 +0530

    crete table

--- employeTable.sql
+++ employeTable.sql
@@ -20,3 +20,21 @@ VALUES
     '2024-11-01',
     1
 );
+
+
+
+
+
+CREATE TABLE Employees (
+    EmployeeId INT IDENTITY PRIMARY KEY,
+    FirstName NVARCHAR(50),
+    LastName NVARCHAR(50),
+    Email NVARCHAR(100),
+    Phone NVARCHAR(20),
+    Department NVARCHAR(50),
+    Designation NVARCHAR(50),
+    DateOfJoining DATE,
+    IsActive BIT
+)
+
+

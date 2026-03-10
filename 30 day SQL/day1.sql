ac0d96c68fe9f2536e3a7c5e64329f992d4e38a4
saujanyamahajan <saujanya.9919@gmail.com>
2026-03-10 10:00:00 +0530

    day1

--- 30 day SQL/day1.sql
+++ 30 day SQL/day1.sql
@@ -1,3 +1,13 @@
+/* Problem Statement:
+- For pairs of brands in the same year (e.g. apple/samsung/2020 and samsung/apple/2020) 
+    - if custom1 = custom3 and custom2 = custom4 : then keep only one pair
+
+- For pairs of brands in the same year 
+    - if custom1 != custom3 OR custom2 != custom4 : then keep both pairs
+
+- For brands that do not have pairs in the same year : keep those rows as well
+*/
+
 with CTE as (
 select *,
 case when brand1 < brand2 then concat(brand1,brand2,year)

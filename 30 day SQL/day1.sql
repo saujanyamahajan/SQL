87fbda09c81845ae84ec72fb5ec86b7b2c80e2ac
saujanyamahajan <saujanya.9919@gmail.com>
2026-03-10 10:00:00 +0530

    day1

--- 30 day SQL/day1.sql
+++ 30 day SQL/day1.sql
@@ -7,17 +7,17 @@
 
 - For brands that do not have pairs in the same year : keep those rows as well
 */
-
+SELECT * from brands;
 with CTE as (
 select *,
 case when brand1 < brand2 then concat(brand1,brand2,year)
  else concat(brand2,brand1,year) end as pair_id
-from brands;)
-
-with CTE2 as (
+from brands),
+-- select * from CTE
+ CTE2 as (
     select *,row_number() over (partition by pair_id order by pair_id) as rn
      from CTE)
-
+--SELECT * from CTE2
 
 select brand1,brand2,year,custom1,custom2,custom3,custom4
 from CTE2

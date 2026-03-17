28168600987c5d3e0e1d1b715a53592165488080
saujanyamahajan <saujanya.9919@gmail.com>
2026-03-17 22:03:30 +0530

    day 8

---
+++ 30 day SQL/day8.sql
@@ -0,0 +1,34 @@
+drop table if exists job_skills;
+create table job_skills
+(
+	row_id		int,
+	job_role	varchar(20),
+	skills		varchar(20)
+);
+insert into job_skills values (1, 'Data Engineer', 'SQL');
+insert into job_skills values (2, null, 'Python');
+insert into job_skills values (3, null, 'AWS');
+insert into job_skills values (4, null, 'Snowflake');
+insert into job_skills values (5, null, 'Apache Spark');
+insert into job_skills values (6, 'Web Developer', 'Java');
+insert into job_skills values (7, null, 'HTML');
+insert into job_skills values (8, null, 'CSS');
+insert into job_skills values (9, 'Data Scientist', 'Python');
+insert into job_skills values (10, null, 'Machine Learning');
+insert into job_skills values (11, null, 'Deep Learning');
+insert into job_skills values (12, null, 'Tableau');
+
+select * from job_skills;
+
+
+
+with cte as(
+select * ,
+sum(case when job_role is null then 0 else 1 end) over (order by row_id) as flag
+FROM job_skills)
+
+
+select row_id,
+FIRST_VALUE(job_role) OVER (PARTITION by flag ORDER by row_id) as job_role_updated,
+skills
+ from cte
